b763974990e08cc425f43c22a8366952eafb1cc1
Beatriz Karoline <beatriz.karoline@ufpe.br>
2023-05-05 17:24:23 -0300

    Proteção de Dados

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 async function conectaBancoDeDados() {
     try{
         console.log('Conexão com o banco de dados iniciou')
 
-        await mongoose.connect('mongodb+srv://helloworld:bOqNBNmVcttLjPo8@clustermulheres.')
+        await mongoose.connect(process.env.MONGO_URL)
 
         console.log('Conexão com o banco de dados feita com sucesso!')
 
